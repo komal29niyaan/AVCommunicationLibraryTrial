865cfd4e223de0cbff117c57e08257db49366be6
komal29niyaan <komalpardeshi3216@gmail.com>
2019-06-18 10:25:53 +0530

    creating a library for avcommunication project app.

---
+++ src/main/java/com/cut2it/cut2itapp/avcommunication/CustomView.java
@@ -0,0 +1,18 @@
+package com.cut2it.cut2itapp.avcommunication;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import com.google.android.exoplayer2.ui.PlayerView;
+
+
+public class CustomView extends PlayerView
+{
+    public CustomView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+
+}
